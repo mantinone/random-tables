e785451f7b0ebc2ca4393ea20f60597d5fbbe739
Max Antinone <mantinone@gmail.com>
2017-11-14 16:24:08 -0800

    Table rolling algorithm complete

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,3 @@
-const rollTable = function( table ) {
-  let probabilitySum = 0
-  let cdfTable = table.map( ( entry ) => probabilitySum += entry.probability )
-  return cdfTable
-}
-
-const randomRange = function ( max, min=1 ){
-  let size = max - min
-  return Math.floor( Math.random()*(size + 1) ) + min
-}
-
 document.addEventListener( "DOMContentLoaded", function(event) {
-  console.log('Dom content loaded worked');
+
 })
--- public/js/testData.js
+++ public/js/testData.js
@@ -11,14 +11,14 @@ const locationEncounters = []
 const creatureEncounters = []
 
 const smallTable = []
-smallTable.push( new Entry( 1, "A weight 1 apple") )
-smallTable.push( new Entry(2, "A weight 2 orange") )
-smallTable.push( new Entry(2, "A weight 2 banana bunch") )
-smallTable.push( new Entry(1, "A weight 1 peach") )
+smallTable.push( new Entry( 1, "1 apple") )
+smallTable.push( new Entry(2, " 2 orange") )
+smallTable.push( new Entry(4, " 40  banana bunch") )
+smallTable.push( new Entry(1, "1 peach") )
 
 const encounterChances = []
 encounterChances.push( new Entry( 12, "No Encounter") )
 encounterChances.push( new Entry( 3, "LocationEncounter", locationEncounters) )
-encounterChances.push( new Entry( 2, "No Encounter", creatureEncounters) )
-encounterChances.push( new Entry( 3, "No Encounter", [locationEncounters, creatureEncounters]) )
+encounterChances.push( new Entry( 2, "Creature Encounter", creatureEncounters) )
+encounterChances.push( new Entry( 3, "Both", [locationEncounters, creatureEncounters]) )
 
