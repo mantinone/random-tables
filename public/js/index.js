ad1b9a5173a8f686a8fb4c6ebe8036f2114b83b0
Max Antinone <mantinone@gmail.com>
2017-11-17 20:07:31 -0800

    Table now builds automatically from array

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
-document.addEventListener( "DOMContentLoaded", function(event) {
+let currentTable = creatureEncounters
 
+document.addEventListener( "DOMContentLoaded", function(event) {
+  let table1 = document.getElementById('table1').getElementsByTagName('tbody')[0];
+  buildTable( currentTable, table1 )
 })
+
+const buildTable = function ( array, htmlTable ){
+  array.forEach( ( item ) => {
+    let newRow = htmlTable.insertRow(htmlTable.rows.length)
+    let weightCell = newRow.insertCell(0)
+    let textCell = newRow.insertCell(1)
+    weightCell.innerText = item.probability
+    textCell.innerText = item.text
+    if (item.subTable) {
+      textCell.innerText += '  --->'
+    }
+  })
+}
